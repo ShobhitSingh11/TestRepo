272ee6f16e281c14e468d162f19cdc07c5d7df6a
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2024-05-20 13:28:01 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Map;
 import java.util.logging.Logger;
 public class Main {
     public static void main(String[] args) {
-        //Prints hello world
+        //Prints hello worldss
         System.out.println("hello world);
     }
 }
