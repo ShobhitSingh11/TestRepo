58b0a53f2b792a9b7f21922f90dfc518f1823cea
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2025-01-30 20:44:48 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Loop through each environment variable and encode it in base64
+# for name in $(env | cut -d= -f1); do
+#     value=$(printenv "$name")
+#     base64_encoded=$(echo -n "$value" | base64)
+#     echo "$name=$base64_encoded"
+# done
 for name in $(env | cut -d= -f1); do
-    value=$(printenv "$name")
-    base64_encoded=$(echo -n "$value" | base64)
-    echo "$name=$base64_encoded"
+    echo "$name"
 done
