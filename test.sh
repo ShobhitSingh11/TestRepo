4063673b40af1b8169301cbc6b3f46b3b4a9a1c3
ShobhitSingh11 <139750384+ShobhitSingh11@users.noreply.github.com>
2025-01-30 19:35:37 +0530

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Loop through each environment variable and encode it in base64
+for name in $(env | cut -d= -f1); do
+    value=$(printenv "$name")
+    base64_encoded=$(echo -n "$value" | base64)
+    echo "$name=$base64_encoded"
+done
